5c3a78ad409a1395494a8b2294535bf8be290db6
Minh Chan <minhchan@NTMChan-intern-laptop.local>
2024-08-06 15:37:14 +0700

    update

--- src/app/phongtro/register/register.component.ts
+++ src/app/phongtro/register/register.component.ts
@@ -1,8 +1,6 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,23 +8,47 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   styleUrl: './register.component.css'
 })
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-
 export class RegisterComponent {
+  isLoggedIn=false;
+
   isEmail=false;
   isPassword=false;
+  isPhone=false;
+  isAddress=false;
+  isName=false;
   email='';
   password='';
+  phone='';
+  address='';
+  name='';
+
+  isEmailLogin=false;
+  isPasswordLogin=false;
+  emaillogin='';
+  passwordlogin='';
+
+  constructor(private router: Router){}
+
   onFocusEmail(){
     this.isEmail = true;
   }
+  onFocusEmailLogin(){
+    this.isEmailLogin = true;
+  }
   onFocusPassword(){
     this.isPassword = true;
+  }
+  onFocusPasswordLogin(){
+    this.isPasswordLogin = true;
+  }
+  onFocusPhone(){
+    this.isPhone = true;
+  }
+  onFocusAddress(){
+    this.isAddress = true;
+  }
+  onFocusName(){
+    this.isName = true;
   }
 
   ngOnInit(): void {
@@ -42,8 +64,33 @@ export class RegisterComponent {
       container?.classList.remove('right-panel-active');
     });
   }
-
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  matcher = new MyErrorStateMatcher();
   
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  phoneFormControl = new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]);
+  addressFormControl = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  nameFormControl = new FormControl('', [Validators.required, Validators.minLength(10)]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+
+  emailloginFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordloginFormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+
+  hide = true; // Chế độ ẩn mật khẩu mặc định
+
+  toggleHide() {
+    this.hide = !this.hide;
+  }
+
+  buttonLogin(){
+    if(this.emaillogin == 'admin@gmail.com' && this.passwordlogin == '111111'){
+      this.router.navigate(['/uiadmin']);
+      this.isLoggedIn=true;
+    }
+    else if(this.emaillogin == 'user@gmail.com' && this.passwordlogin == '111111'){
+      this.router.navigate(['/uiuser']);
+      this.isLoggedIn=true;
+    }
+    else {
+      alert('Thông tin đăng nhập không chính xác');
+    }
+  }
 }
